800548272b3795858805bbb83a7bc757b5a379ee
enprava <epradavazquez@gmail.com>
2023-07-06 14:01:18 +0200

    Update package-lock.json, package.json, and index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import Map from './Map';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Map />
+        <App />
     </React.StrictMode>,
     document.getElementById('body')
 );
